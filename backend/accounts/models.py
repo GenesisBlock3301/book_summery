14a2d44830843c76734fdeee61f0f1f62a9d8a34
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-02-22 15:52:39 +0600

    reformat project structure

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, password: str):
+        """
+        Create general user method
+        """
+        if not email:
+            raise ValueError('Email must be specified!')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+    def create_superuser(self, email: str, password: str):
+        """
+        Create superuser method
+        """
+        if not email:
+            raise ValueError('Email must be specified!')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    """Custom user model"""
+
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=255,
+        unique=True,
+    )
+    password = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def __str__(self):
+        return self.email
+
+
